3b9f9f81b563cba405ec1c8948a5cbb468d5980c
Jeremy Boggs <jeremy@clioweb.org>
2011-07-27 12:06:48 -0400

    Updates them to use HTML5 DOCTYPE.
    Reorganizes some stuff in the header.

--- header.php
+++ header.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
 <title><?php dynamictitles(); ?></title>
 
-<link href='http://fonts.googleapis.com/css?family=Crimson+Text:regular,regularitalic,600,600italic,bold,bolditalic' rel='stylesheet' type='text/css'>
-
+<!-- Style sheets -->
+<link href="http://fonts.googleapis.com/css?family=Crimson+Text:regular,regularitalic,600,600italic,bold,bolditalic" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
 <?php if(is_front_page()) : ?><link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_url'); ?>/style_home.css" /> <?php endif; ?>
 
-<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
-
+<!-- Feeds and Pingbacks -->
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
