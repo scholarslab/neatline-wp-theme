8305cf6f132bb2e973dc37568c65bdf72adb276d
Jeremy Boggs <jeremy@clioweb.org>
2011-10-31 16:56:38 -0400

    Updates plugin info links
    Displays 'In Progress' if a plugin does not have a download URL.
    
    Adds a link for a plugin's Github page. Requires that the Neatline site
    include a post_meta field called github_page.

--- loop_plugin.php
+++ loop_plugin.php
@@ -7,6 +7,7 @@
 $video = get_post_meta( $post->ID , 'plugin_video' , true );
 $video_link = get_post_meta( $post->ID , 'plugin_video-link' , true );
 $download = get_post_meta( $post->ID , 'plugin_download' , true );
+$github = get_post_meta( $post->ID , 'plugin_github_page' , true );
 $image = wp_get_attachment_image_src ( get_post_thumbnail_id ( $post->ID ), 'full' ) ;
 $resized_image = get_bloginfo('template_directory').'/scripts/timthumb.php?src='.$image[0].'&amp;w=230&amp;h=170&amp;q=100&amp;zc=1';
 $more_info = get_post_meta( $post->ID , 'plugin_additional' , true );
@@ -30,10 +31,15 @@ $more_info = get_post_meta( $post->ID , 'plugin_additional' , true );
                         <?php endif; ?>
                         <?php the_content(); ?>
 <?php endif; ?>
-<?php if($download || $video_link) : ?>
                         <?php if($video_link) : ?><a href="<?php echo $video_link; ?>" title="<?php the_title_attribute(); ?>" class="colorbox-link more download">View Larger Video</a><?php endif; ?>
-                        <?php if($download) : ?><a href="<?php echo $download; ?>" title="Download <?php the_title_attribute(); ?>" class="more download">Download</a><?php endif; ?>
-<?php endif; ?>
+                        <?php if($download) : ?>
+                            <a href="<?php echo $download; ?>" title="Download <?php the_title_attribute(); ?>" class="more download">Download</a>
+                        <?php else: ?>
+                            <span>In Progress</span>
+                        <?php endif; ?>
+                        <?php if($github) : ?>
+                            <a href="<?php echo $github; ?>" class="more github">Follow Development on Github</a>
+                        <?php endif; ?>
 <?php if($more_info) : ?>
                         <div class="info">
                         <?php echo $more_info; ?>
