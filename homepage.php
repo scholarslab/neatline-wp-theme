90ae21791378d998e597f61ae5399be168d3aeee
Jeremy Boggs <jeremy@clioweb.org>
2012-02-16 13:46:30 -0500

    Updates markup and styles for homepage.
    Adds buttons for learing more about Neatline, and trying the web service.
    
    Adds better way of doing beveled borders for various elements on site.

--- homepage.php
+++ homepage.php
@@ -23,15 +23,21 @@ if ($neatlinePlugins->have_posts()) : ?>
 <div id="screenshots" class="container">
 <?php while ($neatlinePlugins->have_posts()) : $neatlinePlugins->the_post(); ?>
 
-<a class="screenshot" href="<?php the_permalink(); ?>">
+<a class="screenshot" id="<?php echo str_replace(' ', '-', strtolower(get_the_title())); ?>" href="<?php the_permalink(); ?>">
   <h2><?php the_title(); ?></h2>
   <?php if (has_post_thumbnail()) {
       the_post_thumbnail('fullsize');
     }
   ?>
-  <?php the_excerpt(); ?>
+  <div class="description"><?php the_excerpt(); ?></div>
 </a>
-
+<?php if ($post->menu_order == '1'): ?>
+<ul id="neatline-meta-links">
+  <li id="neatline-learn-more"><a href="<?php the_permalink(); ?>" class="button">Learn more about Neatline</a></li>
+  <li id="neatline-try-it"><a href="http://neatline.scholarslab.org" class="button">Try It!</a></li>
+</ul>
+<p id="separate-components-note">You can also use Neatline components separately.</p>
+<?php endif; ?>
 <?php endwhile; ?>
 </div>
 
