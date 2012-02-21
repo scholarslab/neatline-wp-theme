004a430392cde6ecdb145efdfbcde4323d65654f
Jeremy Boggs <jeremy@clioweb.org>
2012-02-21 10:51:13 -0500

    Removes legacy code for getting child pages.

--- homepage.php
+++ homepage.php
@@ -43,39 +43,4 @@ if ($neatlinePlugins->have_posts()) : ?>
 
 <?php wp_reset_query(); endif; ?>
 
-<?php
-/*
-// Get the page object for "Neatline in Action"
-$inActionPage = get_page_by_title( 'Neatli In ');
-if ($inActionPage):
-    
-    // Query WP for all the child pages for "Neatline in Action"
-    $inActionChildQuery = new WP_Query('static=true&post_status=publish&posts_per_page=-1&post_parent='.$inActionPage->ID.'&orderby=menu_order&order=ASC');
-    
-    // If we get some results, we'll loop through them like semi-hot butter.
-    if($inActionChildQuery->have_posts()) : ?>
-        <div id="neatline-in-action">
-            <h2>Neatline In Action</h2>
-        <?php while ($inActionChildQuery->have_posts()) : $inActionChildQuery->the_post(); ?>
-            <div class="entry">
-            <?php
-            // If the child page has a thumbnail, we should display that goodness.
-            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-              the_post_thumbnail();
-            }
-            ?>
-            <div class="text">
-                <h2><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-                <?php the_content(); ?>
-                <a class="more" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">Learn More</a>
-            </div>
-        </div>
-
-        <?php endwhile; ?>
-        </div>
-    <?php endif; ?>
-    <?php wp_reset_query(); ?>
-    </div>
-<?php endif; */ ?>
-
 <?php get_footer(); ?>
