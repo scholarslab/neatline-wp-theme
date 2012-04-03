f4801070bd09b95d3e3fb0c0805856ec076a1547
Jeremy Boggs <jeremy@clioweb.org>
2012-04-03 09:10:30 -0400

    Adds Modernizr and friends.
    Also some minor cleanup in header.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html lang="en"><!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title><?php dynamictitles(); ?></title>
 <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/favicon.png">
@@ -17,26 +17,27 @@
 <link href="http://fonts.googleapis.com/css?family=Crimson+Text:regular,regularitalic,600,600italic,bold,bolditalic" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
-<!-- enable HTML5 elements in IE7+8 --> 
-<!--[if lt IE 9]>
-<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-
 <!-- Feeds and Pingbacks -->
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
-<?php
-if (is_page('Homepage')) {
-    // wp_enqueue_script('neatline-scripts', get_bloginfo('template_url') . '/scripts.js', array('jquery'),null, true);
-}
-?>
+<!-- Modernizr and Friends -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/javascripts/modernizr.min.js"></script>
+<script>
+  Modernizr.load([
+    {
+      test: Modernizr.mq(),
+      nope: ['<?php echo get_stylesheet_directory_uri(); ?>/javascripts/respond.min.js',
+      '<?php echo get_stylesheet_directory_uri(); ?>/javascripts/selectivizr.min.js']
+    }
+  ]);
+</script>
+
 <?php wp_head(); ?>
 
 </head>
 
-<body <?php echo body_class(); ?>>
+<body <?php body_class(); ?>>
 
 <header role="banner" class="container">
 
