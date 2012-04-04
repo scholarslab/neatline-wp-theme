88363f42a631877bd8972f80c44332be7d083206
Jeremy Boggs <jeremy@clioweb.org>
2012-04-04 09:09:37 -0400

    Adds Scholars' Lab footer.
    Removes duplicate img styles.

--- footer.php
+++ footer.php
@@ -4,7 +4,12 @@
         <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?><?php endif; ?>
     </div>
     <div id="slab-info">
-        <a id="slab-logo" href="http://www.scholarslab.org/"><img alt="Scholars' Lab" src="<?php bloginfo('template_url'); ?>/images/logo_scholarslab.png" border="0"  /></a>
+      <ul>
+        <li><a rel=license class=license href="http://creativecommons.org/licenses/by/3.0/">Creative Commons License</a></li>
+        <li><a href="http://www.scholarslab.org/category/praxis-program/feed/" class=feed>Subscribe to our Feed</a></li>
+        <li><a class=email href="#">Contact Us</a></li>
+      </ul>
+      <a id="slab-logo" href="http://www.scholarslab.org/"><img src="http://static.scholarslab.org/images/slab-logo-rgb-125px.png" title="Scholars' Lab" alt="Scholars' Lab"/></a>
     </div>
     <?php wp_footer(); ?>
 </footer><!--/footer-->
