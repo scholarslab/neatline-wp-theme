f68e3095c7e34c5bfe45fdbe9f6052f5cdfd5b3c
Jeremy Boggs <jeremy@clioweb.org>
2012-04-04 09:08:06 -0400

    Updates display of plugins in loop.
    Removes timthumb script.

--- loop_plugin.php
+++ loop_plugin.php
@@ -1,7 +1,4 @@
-<?php if ($alt=='') { ?>
-            <div class="row clearfloat">
-<?php } ?>
-                <div class="entry entry-full<?php if ($alt=='') { $alt = 'alt'; } else { echo ' '.$alt; $alt = ''; } ?>">
+<article class="<?php post_class(); ?>">
 
 <?php
 $video = get_post_meta( $post->ID , 'plugin_video' , true );
@@ -9,50 +6,33 @@ $video_link = get_post_meta( $post->ID , 'plugin_video-link' , true );
 $download = get_post_meta( $post->ID , 'plugin_download' , true );
 $github = get_post_meta( $post->ID , 'plugin_github_page' , true );
 
-
-$image = wp_get_attachment_image_src ( get_post_thumbnail_id ( $post->ID ), 'full' ) ;
-$resized_image = get_bloginfo('template_directory').'/scripts/timthumb.php?src='.$image[0].'&amp;w=230&amp;h=170&amp;q=100&amp;zc=1';
+$image = wp_get_attachment_image_src(get_post_thumbnail_id( get_the_ID() ), 'full' ) ;
+$image = $image[0];
 $more_info = get_post_meta( $post->ID , 'plugin_additional' , true );
 ?>
 
-<?php if ( has_post_thumbnail() ) : ?>
-                    <img src="<?php echo $resized_image; ?>" alt="<?php the_title(); ?>" class="alignright" />
+<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+<?php if ($image) : ?>
+<img src="<?php echo $image; ?>" alt="<?php the_title(); ?>" />
 <?php endif; ?>
-
-                    <div class="text<?php if(!has_post_thumbnail()) : ?> full-text<?php endif; ?>">
-<?php if(is_single()) : ?>
-                        <h2 class="posttitle"><?php the_title(); ?></h2>
-                        <?php if($video) : ?>
-                            <?php echo ( do_shortcode( get_post_meta( $post->ID , 'plugin_video' , true ) ) ); ?>
-                        <?php endif; ?>
-                        <?php the_content(); ?>
-<?php else : ?>
-                        <h3><?php the_title(); ?></h3>
-                        <?php if($video) : ?>
-                            <?php echo do_shortcode($video); ?>
-                        <?php endif; ?>
-                        <?php the_content(); ?>
+<div class="content">
+  <?php the_excerpt(); ?>
+  <?php if($more_info) : ?>
+    <div class="info">
+    <?php echo $more_info; ?>
+    </div>
+  <?php endif; ?>
+</div>
+<div class="plugin-links">
+<?php if($download) : ?>
+<span><a href="<?php echo $download; ?>" title="Download <?php the_title_attribute(); ?>" class="more download">Download</a></span>
+<?php else: ?>
+<span>In Progress</span>
 <?php endif; ?>
-                    <div class="plugin-links">
-                        <?php if($video_link) : ?><span><a href="<?php echo $video_link; ?>" title="<?php the_title_attribute(); ?>" class="colorbox-link more download">View Larger Video</a></span><?php endif; ?>
-                        <?php if($download) : ?>
-                            <span><a href="<?php echo $download; ?>" title="Download <?php the_title_attribute(); ?>" class="more download">Download</a></span>
-                        <?php else: ?>
-                            <span>In Progress</span>
-                        <?php endif; ?>
-                        <?php if($github) : ?>
-                            <span><a href="<?php echo $github; ?>" class="more github">Follow <?php the_title(); ?> on Github</a></span>
-                        <?php endif; ?>
-                    </div>
-<?php if($more_info) : ?>
-                        <div class="info">
-                        <?php echo $more_info; ?>
-                        </div>
+<?php if($github) : ?>
+<span><a href="<?php echo $github; ?>" class="more github">Follow <?php the_title(); ?> on Github</a></span>
 <?php endif; ?>
-                    </div>
+</div>
 
-                </div>
+</article>
 
-<?php if ($alt=='') { ?>
-            </div><!--/row-->
-<?php } ?>
