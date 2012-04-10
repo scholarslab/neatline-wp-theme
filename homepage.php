3f750136bf73a3e269788e51719e2fc86b248daf
Jeremy Boggs <jeremy@clioweb.org>
2012-04-10 09:18:21 -0400

    Updates homepage content and markup.
     * Adds See It button under Neatline, for Neatline exhibits.
     * Adds more descriptive titles to Neatline buttons.
     * Uses homepage caption custom post field in plugin list.

--- homepage.php
+++ homepage.php
@@ -29,12 +29,16 @@ if ($neatlinePlugins->have_posts()) : ?>
       the_post_thumbnail('fullsize');
     }
   ?>
-  <div class="description"><?php the_excerpt(); ?></div>
+
+<?php if($pluginCaption = get_post_meta( $post->ID , 'plugin_homepage_caption' , true )) : ?>
+<div class="description"><?php echo $pluginCaption; ?></div>
+<?php endif; ?>
 </a>
 <?php if ($post->menu_order == '1'): ?>
 <ul id="neatline-meta-links">
-  <li id="neatline-learn-more"><a href="<?php the_permalink(); ?>" class="button">Get it!</a></li>
-  <li id="neatline-try-it"><a href="http://neatline.scholarslab.org" class="button">Try It!</a></li>
+  <li id="neatline-learn-more"><a href="<?php the_permalink(); ?>" class="button" title="Learn more about the Neatline plugin.">Get It!</a></li>
+  <li id="neatline-try-it"><a href="http://neatline.scholarslab.org" class="button" title="Try out Neatline.">Try It!</a></li>
+  <li id="neatline-see-it"><a href="#" class="button" title="View some example exhibits of Neatline in action.">See It!</a></li>
 </ul>
 <p id="separate-components-note">You can also use Neatline components separately.</p>
 <?php endif; ?>
