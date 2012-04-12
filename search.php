8906c95351f737f9a2211a36d5c8b2eb65ae5c9c
Jeremy Boggs <jeremy@clioweb.org>
2012-04-12 13:13:04 -0400

    Fixes search page title.

--- search.php
+++ search.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
 <div id="primary">
-<h1 class="posttitle">Search</h1>
-
-    <h2>Search Results for "<em><?php the_search_query() ?></em>"</h2>
+<h1>Search Results for &#8220;<em><?php the_search_query() ?></em>&#8221;</h2>
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
