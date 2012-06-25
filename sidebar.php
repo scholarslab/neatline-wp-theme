ad6e014dc5c95671829d5c6296d1a9a5079f47c8
Jeremy Boggs <jeremy@clioweb.org>
2012-06-25 15:10:45 -0400

    Syntax error.

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 if ( function_exists('dynamic_sidebar') ) {
   if (is_page('About')) {
     dynamic_sidebar('About Page Sidebar'); 
-  } else if (is_single()) }
+  } else if (is_single()) {
     dynamic_sidebar('Single Post');
   } else {
     dynamic_sidebar('Blog Sidebar');
