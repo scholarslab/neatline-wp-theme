d250d58e6a58caf2d8c2c9cdef803c90c617583c
Wayne Graham <wayne.graham@virginia.edu>
2012-07-02 11:36:08 -0400

    Fixes email link

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
       <ul>
         <li><a rel="license" class="license" href="http://creativecommons.org/licenses/by/3.0/">CC-BY 3.0</a></li>
         <li><a href="<?php bloginfo('atom_url'); ?>" class="feed">Atom</a> · <a href="<?php bloginfo('rss2_url'); ?>">RSS</a></li>
-        <li><a class=email href="&#110;&#101;&#97;&#116;&#108;&#105;&#110;&#101;&#64;&#99;&#111;&#108;&#108;&#97;&#98;&#46;&#105;&#116;&#99;&#46;&#118;&#105;&#114;&#103;&#105;&#110;&#105;&#97;&#46;&#101;&#100;&#117;">Contact Us</a></li>
+        <li><a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#110;&#101;&#97;&#116;&#108;&#105;&#110;&#101;&#64;&#99;&#111;&#108;&#108;&#97;&#98;&#46;&#105;&#116;&#99;&#46;&#118;&#105;&#114;&#103;&#105;&#110;&#105;&#97;&#46;&#101;&#100;&#117;'>&#67;&#111;&#110;&#116;&#97;&#99;&#116;&#32;&#85;&#115;</a></li>
       </ul>
       <a id="slab-logo" href="http://www.scholarslab.org/"><img src="http://static.scholarslab.org/images/logos/slab/slab-logo-black-200px.png" title="Scholars' Lab" alt="Scholars' Lab"/></a>
     </div>
