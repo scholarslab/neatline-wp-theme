e08044c0032b7bb323ef0c8b28d3a45206b2c1e3
Wayne Graham <wayne.graham@virginia.edu>
2012-07-03 16:15:44 -0400

    Adds analytics beacon

--- footer.php
+++ footer.php
@@ -12,6 +12,9 @@
       <a id="slab-logo" href="http://www.scholarslab.org/"><img src="http://static.scholarslab.org/images/logos/slab/slab-logo-black-200px.png" title="Scholars' Lab" alt="Scholars' Lab"/></a>
     </div>
     <?php wp_footer(); ?>
+<script type="text/javascript">
+var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-33089897-1"]),_gaq.push(["_trackPageview"]),function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}()
+</script>
 </footer><!--/footer-->
 </body>
 </html>
